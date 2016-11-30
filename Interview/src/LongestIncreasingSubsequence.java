6528911090454e4e44b0afc1cec612d0a328b94c
dilip-devaraj <ddevaraj@ddevaraj-ltm.internal.salesforce.com>
2016-11-30 13:34:01 -0800

    Corrected solution for longest increasing subsequence that looks back at all previous numbers

--- Interview/src/LongestIncreasingSubsequence.java
+++ Interview/src/LongestIncreasingSubsequence.java
@@ -4,31 +4,26 @@ public class LongestIncreasingSubsequence {
 	public static int longestIncSubSeq(int[] numbers){
 
 		int[] dp = new int[numbers.length];
-		int[] parent = new int[numbers.length];
-		dp[0] = 1;
 
+		// Each number starts with longest sub seq length corresponding to itself.
+		for(int i=0; i< numbers.length; i++){
+			dp[i] = 1;
+		}
 
-		for(int i=0; i< numbers.length -1 ; i++){
-			if(numbers[i+1] > numbers[i]) {
-				dp[i+1] = dp[i]+1;
-				parent[i+1] = i;
-			} else {
-				dp[i+1] = dp[i];
-				parent[i+1] = -1;
+		for(int i=1; i< numbers.length; i++){
+			for(int j=0; j<i; j++) {
+				if(numbers[i] >= numbers[j] && dp[i] < dp[j]+1) {
+					dp[i] = dp[j]+1;
+				}
 			}
 		}
-		
-/*		for(int i= numbers.length-1; i>=0; i--){
-			if(parent[i] != -1){
-				System.out.print(numbers[i] + " ");
-			}
-		}*/
+
 		return dp[numbers.length -1];
 	}
 
 	public static void main(String[] args){
-		int[] numbers= {1,6,5,2,-3, -2, -1,3,4,5};
-
+	//  int[] numbers= {1,6,5,2,-3, -2, -1,3,4,5};
+	  int[] numbers= {5,3,4,8,6,7};
 		System.out.println(longestIncSubSeq(numbers));
 	}
 }
