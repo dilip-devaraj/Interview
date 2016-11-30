ff9dd438b5e93352c839bbf63fabcd3b04d10d78
dilip-devaraj <ddevaraj@ddevaraj-ltm.internal.salesforce.com>
2016-11-30 13:52:34 -0800

    Add sequence of numbers in addition to length

--- Interview/src/LongestIncreasingSubsequence.java
+++ Interview/src/LongestIncreasingSubsequence.java
@@ -4,26 +4,36 @@ public class LongestIncreasingSubsequence {
 	public static int longestIncSubSeq(int[] numbers){
 
 		int[] dp = new int[numbers.length];
+		int[] parent = new int[numbers.length];
 
 		// Each number starts with longest sub seq length corresponding to itself.
 		for(int i=0; i< numbers.length; i++){
 			dp[i] = 1;
+			parent[i] = i;
 		}
 
 		for(int i=1; i< numbers.length; i++){
 			for(int j=0; j<i; j++) {
 				if(numbers[i] >= numbers[j] && dp[i] < dp[j]+1) {
 					dp[i] = dp[j]+1;
+					parent[i] = j;
 				}
 			}
 		}
 
+		int i=numbers.length -1;
+		do{
+			System.out.print(numbers[i] + " ");
+			i = parent[i];			
+		}while (parent[i] != i);
+		System.out.println(numbers[i]);
+		
 		return dp[numbers.length -1];
 	}
 
 	public static void main(String[] args){
-	//  int[] numbers= {1,6,5,2,-3, -2, -1,3,4,5};
-	  int[] numbers= {5,3,4,8,6,7};
+		int[] numbers= {1,6,5,2,-3, -2, -1,3,4,5};
+		// int[] numbers= {5,3,4,8,6,7};
 		System.out.println(longestIncSubSeq(numbers));
 	}
 }
