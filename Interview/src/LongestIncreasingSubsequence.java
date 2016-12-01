02522efef9576e5353cefc6d691df8f9ec2a5122
dilip-devaraj <ddevaraj@ddevaraj-ltm.internal.salesforce.com>
2016-11-30 16:15:06 -0800

    Use stack to print numbers in correct order

--- Interview/src/LongestIncreasingSubsequence.java
+++ Interview/src/LongestIncreasingSubsequence.java
@@ -1,17 +1,21 @@
+import java.util.Stack;
 
 public class LongestIncreasingSubsequence {
 
 	public static int longestIncSubSeq(int[] numbers){
-
+        Stack<Integer> numStack = new Stack<Integer>();
 		int[] dp = new int[numbers.length];
 		int[] parent = new int[numbers.length];
-
+		int longestLengthEndingIndex = 0;
+		int longestLength = 0;
+		
 		// Each number starts with longest sub seq length corresponding to itself.
 		for(int i=0; i< numbers.length; i++){
 			dp[i] = 1;
 			parent[i] = i;
 		}
 
+		
 		for(int i=1; i< numbers.length; i++){
 			for(int j=0; j<i; j++) {
 				if(numbers[i] >= numbers[j] && dp[i] < dp[j]+1) {
@@ -19,16 +23,25 @@ public class LongestIncreasingSubsequence {
 					parent[i] = j;
 				}
 			}
+			if(dp[i] > longestLength) {
+				longestLength= dp[i];
+				longestLengthEndingIndex = i;
+			}
 		}
 
-		int i=numbers.length -1;
-		do{
-			System.out.print(numbers[i] + " ");
+		int i=longestLengthEndingIndex;
+		while (parent[i] != i){
+			numStack.push(numbers[i]);
 			i = parent[i];			
-		}while (parent[i] != i);
-		System.out.println(numbers[i]);
+		}
+		numStack.push(numbers[i]);
+	
+		while(! numStack.isEmpty()){
+			System.out.print(numStack.pop() + " ");
+		}
 		
-		return dp[numbers.length -1];
+		System.out.println();
+		return dp[longestLengthEndingIndex];
 	}
 
 	public static void main(String[] args){
